c05d722f65806f20fa3bd9ed079f547b51e376d8
JOHN D NEWTON <nujdn4@MLRBQY24G6QJG5H.local>
2020-10-13 21:54:13 -0400

    commit testing some changes

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('test', { title: 'Cold Chain Tracking' });
+  res.render('test', { 
+      title: 'Cold Chain Tracking',
+      authors: ['Jack Newton', 'Cam Harvey', 'Calvin Lee'],
+      body: 'Welcome to our Cold Chain Tracking solution for Senior Design Project 2021.'
+    });
 });
 
 module.exports = router;
