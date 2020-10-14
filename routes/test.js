fdde02ebdf9f412f8a9c4b04ff6ee5c78c79b471
JOHN D NEWTON <nujdn4@MLRBQY24G6QJG5H.local>
2020-10-13 21:44:17 -0400

    commit 5

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('test', { title: 'SDP-21 Cold Chain Tracking' });
+  res.render('test', { title: 'Cold Chain Tracking' });
 });
 
 module.exports = router;
