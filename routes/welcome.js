c339d6ba9550f65333c76302b2aa9730ad60ce35
jnewton-umass <jnewton@umass.edu>
2020-10-18 12:24:19 -0400

    welcome changes

--- routes/welcome.js
+++ routes/welcome.js
@@ -3,10 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  console.log(message, req.body.username);
   res.render('welcome', {
-      login: req.query.username,
-      password: req.query.password
+      login: req.body.username,
+      password: req.body.password
   });
 });
 
