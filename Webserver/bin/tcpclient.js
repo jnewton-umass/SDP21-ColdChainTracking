ce29267a43e345453d3a9611ede4f6308bdc8153
newtonj64 <nujdn4@us.royalahold.net>
2021-03-06 14:30:28 -0500

    tcp server

---
+++ Webserver/bin/tcpclient.js
@@ -0,0 +1,26 @@
+var net = require('net');
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+  });
+var client = new net.Socket();
+client.connect(8081, 'localhost', function() {
+	console.log('Connected');
+	client.write('Hello, server! Love, Client.');
+	readline.question('Response: ', res => {
+		client.write(res);
+		readline.close();
+	});
+});
+
+client.on('data', function(data) {
+	console.log('Received: ' + data);
+	readline.question('Response: ', res => {
+		client.write(res);
+		readline.close();
+	});
+});
+
+client.on('close', function() {
+	console.log('Connection closed');
+});
