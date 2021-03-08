a6554b71f4f6ef4991ddc75afb33eae452485870
newtonj64 <nujdn4@us.royalahold.net>
2021-03-08 08:54:39 -0500

    ip address changes

--- Webserver/bin/tcpclient.js
+++ Webserver/bin/tcpclient.js
@@ -4,21 +4,14 @@ const readline = require('readline').createInterface({
 	output: process.stdout
   });
 var client = new net.Socket();
-client.connect(8081, 'possible-point-294420.ue.r.appspot.com', function() {
+client.connect(8081, '35.190.148.56', function() {
 	console.log('Connected');
 	client.write('Hello, server! Love, Client.');
-	readline.question('Response: ', res => {
-		client.write(res);
-		readline.close();
-	});
 });
 
 client.on('data', function(data) {
 	console.log('Received: ' + data);
-	readline.question('Response: ', res => {
-		client.write(res);
-		readline.close();
-	});
+	client.write('OK\n');
 });
 client.on('error', function(error) {
 	console.log(error);
