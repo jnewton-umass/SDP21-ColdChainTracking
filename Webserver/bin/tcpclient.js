ff8c108b82a1c93c869d1e8eecb827d6d1a43f94
newtonj64 <nujdn4@us.royalahold.net>
2021-03-11 22:16:44 -0500

    a

--- Webserver/bin/tcpclient.js
+++ Webserver/bin/tcpclient.js
@@ -1,15 +1,16 @@
 var net = require('net');
 var JsonSocket = require('json-socket')
 var client = new JsonSocket(new net.Socket())
+const mes = {
+	statusId: "demoStatus",
+	temperature: 30,
+	light: false,
+	latitude: 42.374376,
+	longitude: -72.519899
+}
 client.connect(8081, '35.190.148.56', function() {
 	console.log('Connected');
-	client.sendMessage({
-		statusId: "demoStatus",
-		temperature: 30,
-		light: false,
-		latitude: 42.374376,
-		longitude: -72.519899
-	});
+	client.sendMessage(JSON.stringify(mes));
 });
 client.on('data', function(data) {
 	console.log('Received: ' + data);
