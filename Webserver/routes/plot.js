0d86daca7ef5f4e385e50d17738169b9e46f1bd3
newtonj64 <nujdn4@us.royalahold.net>
2021-03-29 13:24:59 -0400

    yes

--- Webserver/routes/plot.js
+++ Webserver/routes/plot.js
@@ -1,33 +1,31 @@
 var express = require('express');
 var statusData = require("../models/statusModel.js")
-var deliveredModel = require("../models/deliveredModel")
+var deliveredModel = require("../models/deliveredModel");
+const inTransitModel = require('../models/inTransitModel.js');
 var router = express.Router();
 
 router.post('/', async function(req, res, next) {
-    /*let temp1 = [];
-    let temp2 = [];
-    let lat = [];
-    let lon =  [];*/
     return new Promise(async function (resolve, reject) {
         const filter = {gatewayId: req.body.gatewayId}
-        await deliveredModel.findOne(filter).then(result => {
-            /*console.log(result);
-            result.forEach(el => {
-                temp1.push(el.tempCeil);
-                temp2.push(el.tempSide);
-                lat.push(el.latitude);
-                lon.push(el.longitude);*/
+        await deliveredModel.findOne(filter).then(async result => {
+            if (result.length == 0){
+                await inTransitModel.findOne(filter).then(result => {
+                    return resolve(res.render('plot', {gatewayId: req.body.gatewayId, fiftyPlus: result.fiftyPlus, seventyPlus: result.seventyPlus, eightyPlus: result.eightyPlus, startTime: result.startTime}))
+                })
+                .catch(error => {
+                    console.log(error)
+                    return reject(error)
+                })
+            }
+            else {
                 return resolve(res.render('plot', {gatewayId: req.body.gatewayId, fiftyPlus: result.fiftyPlus, seventyPlus: result.seventyPlus, eightyPlus: result.eightyPlus, startTime: result.startTime}))
-            })
-            .catch(error => {
-                console.log(error)
-                return reject(error)
-            })
-            /*console.log(temp1);
-            console.log(temp2);
-            console.log(lat);
-            console.log(lon);*/
-            
+            }
+                
+        })
+        .catch(error => {
+            console.log(error)
+            return reject(error)
+        })
     })
  })
 
