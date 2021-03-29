3738a50bb3272273711b90ce026f77af853b1afb
newtonj64 <nujdn4@us.royalahold.net>
2021-03-29 12:21:07 -0400

    final

--- Webserver/routes/plot.js
+++ Webserver/routes/plot.js
@@ -1,36 +1,34 @@
 var express = require('express');
 var statusData = require("../models/statusModel.js")
+var deliveredModel = require("../models/deliveredModel")
 var router = express.Router();
 
 router.post('/', async function(req, res, next) {
-    let temp1 = [];
+    /*let temp1 = [];
     let temp2 = [];
     let lat = [];
-    let lon =  [];
+    let lon =  [];*/
     return new Promise(async function (resolve, reject) {
         const filter = {gatewayId: req.body.gatewayId}
-        await statusData.find(filter).then(result => {
-            console.log(result);
+        await deliveredModel.findOne(filter).then(result => {
+            /*console.log(result);
             result.forEach(el => {
                 temp1.push(el.tempCeil);
                 temp2.push(el.tempSide);
                 lat.push(el.latitude);
-                lon.push(el.longitude);
-            });
-            console.log(temp1);
+                lon.push(el.longitude);*/
+                return resolve(res.render('plot', {gatewayId: req.body.gatewayId, fiftyPlus: result.fiftyPlus, seventyPlus: result.seventyPlus, eightyPlus: result.eightyPlus, startTime: result.startTime}))
+            })
+            .catch(error => {
+                console.log(error)
+                return reject(error)
+            })
+            /*console.log(temp1);
             console.log(temp2);
             console.log(lat);
-            console.log(lon);
-            res.render('plot', {tempCeil: temp1, tempSide: temp2, latitude: lat, longitude: lon}, (error, result)  => {
-                console.log(error, result)
-                return resolve(res.render(result))
-            });
-        })
-        .catch(error => {
-            console.log(error)
-            return reject(error)
-        })
+            console.log(lon);*/
+            
     })
-});
+ })
 
 module.exports = router;
