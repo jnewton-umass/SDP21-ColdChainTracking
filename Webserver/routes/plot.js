2e2bb06534dda0738923164473243a42dbe7b318
newtonj64 <nujdn4@us.royalahold.net>
2021-04-04 21:32:00 -0400

    yse

--- Webserver/routes/plot.js
+++ Webserver/routes/plot.js
@@ -9,7 +9,7 @@ router.post('/', async function(req, res, next) {
         const filter = {gatewayId: req.body.gatewayId};
         await deliveredModel.findOne(filter).then(async result => {
             if (result == null){
-                await inTransitModel.findOne(filter).then(async result => {
+                await inTransitModel.findOne(filter).then(async result2 => {
                     /*let longitude =  [];
                     let latitude = [];
                     let tempCeil = [];
@@ -41,7 +41,7 @@ router.post('/', async function(req, res, next) {
                             size: 6
                         }
                     };*/
-                    return resolve(res.render('plot', {gatewayId: req.body.gatewayId, fiftyPlus: result.fiftyPlus, seventyPlus: result.seventyPlus, eightyPlus: result.eightyPlus, startTime: result.startTime}));
+                    return resolve(res.render('plot', {gatewayId: req.body.gatewayId, fiftyPlus: result2.fiftyPlus, seventyPlus: result2.seventyPlus, eightyPlus: result2.eightyPlus, startTime: result2.startTime}));
                 })
                 .catch(error => {
                     console.log(error)
@@ -49,50 +49,9 @@ router.post('/', async function(req, res, next) {
                 })
             }
             else {
-                await inTransitModel.findOne(filter).then(async result => {
-                    /*let longitude =  [];
-                    let latitude = [];
-                    let tempCeil = [];
-                    let tempSide = [];
-                    let avgTemp = [];
-                    let time = [];
-                    await statusData.find(filter).then(result2 => {
-                        result2.forEach(statusUpdate => {
-                            longitude.push(statusUpdate.longitude);
-                            latitude.push(statusUpdate.latitude);
-                            tempCeil.push(statusUpdate.tempCeil);
-                            tempSide.push(statusUpdate.tempSide);
-                            avgTemp.push((statusUpdate.tempCeil + statusUpdate.tempSide)/2.0);
-                            time.push(statusUpdate.updatedAt);
-                        })
-                        var data = [{
-                            type: 'scattergeo',
-                            mode: 'markers',
-                            lon: longitude,
-                            lat: latitude,
-                            marker: {
-                                size: 2
-                            },
-                            name: 'Status Update Points',
-                        }];
-                        var layout = {
-                            title: 'Status Update Points',
-                            font: {
-                                family: 'Droid Serif, serif',
-                                size: 6
-                            }
-                        };
-                        console.log(data);
-                    })*/
-                    return resolve(res.render('plot', {gatewayId: req.body.gatewayId, fiftyPlus: result.fiftyPlus, seventyPlus: result.seventyPlus, eightyPlus: result.eightyPlus, startTime: result.startTime}, function (err, html) {
-                        console.log(html);
-                        res.send(html);
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    }))
-            })
-        }})
+                return resolve(res.render('plot', {gatewayId: req.body.gatewayId, fiftyPlus: result.fiftyPlus, seventyPlus: result.seventyPlus, eightyPlus: result.eightyPlus, startTime: result.startTime}))
+            }
+        })
     })
 })
 module.exports = router;
