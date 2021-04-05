887ae63ec6302eebbf408a7646bf837d47ee7c3a
newtonj64 <nujdn4@us.royalahold.net>
2021-04-04 21:00:01 -0400

    hmmm

--- Webserver/bin/tcpclient.js
+++ Webserver/bin/tcpclient.js
@@ -1,13 +1,12 @@
 var net = require('net');
 var client = new net.Socket()
-const mes1 = 'demoGateway 40.6 45.2 0 42.374376 -72.519899 628021800000'
-const mes2 = 'demoGateway 44.6 45.2 0 42.374376 -72.519899 628021800000'
-const mes3 = 'demoGateway 56.3 52.6 1 42.374376 -72.519899 628021800000'
-const mes4 = 'demoGateway 57.9 57.8 1 42.374376 -72.519899 628021800000'
-const mes5 = 'demoGateway 71.9 70.5 1 42.374376 -72.519899 628021800000'
-const mes6 = 'demoGateway 80.6 80.2 1 42.374376 -72.519899 628021800000'
+const mes1 = 'ThisDemo 40.6 45.2 0 42.374376 -72.519899 628021800000'
+const mes2 = 'ThisDemo 44.6 45.2 0 42.374376 -72.519899 628021800000'
+const mes3 = 'ThisDemo 56.3 52.6 1 42.374376 -72.519899 628021800000'
+const mes4 = 'ThisDemo 57.9 57.8 1 42.374376 -72.519899 628021800000'
+const mes5 = 'ThisDemo 71.9 70.5 1 42.374376 -72.519899 628021800000'
+const mes6 = 'ThisDemo 80.6 80.2 1 42.374376 -72.519899 628021800000'
 const mes = [mes1,mes2,mes3,mes4,mes5,mes6]
-
 client.connect(8081, '35.190.148.56', function() {
 	console.log('Connected');
 	client.on('data', function(data) {
@@ -19,9 +18,6 @@ client.connect(8081, '35.190.148.56', function() {
 	client.on('close', function() {
 		console.log('Connection closed');
 	});
-	client.write(mes[i]);
+	client.write(mes[4]);
 	client.end();
 });
-function transmit() {
-
-}
--- Webserver/routes/plot.js
+++ Webserver/routes/plot.js
@@ -3,14 +3,49 @@ var statusData = require("../models/statusModel.js")
 var deliveredModel = require("../models/deliveredModel");
 const inTransitModel = require('../models/inTransitModel.js');
 var router = express.Router();
-
+var  Plotly = require('plotly');
 router.post('/', async function(req, res, next) {
     return new Promise(async function (resolve, reject) {
-        const filter = {gatewayId: req.body.gatewayId}
+        const filter = {gatewayId: req.body.gatewayId};
         await deliveredModel.findOne(filter).then(async result => {
             if (result == null){
-                await inTransitModel.findOne(filter).then(result => {
-                    return resolve(res.render('plot', {gatewayId: req.body.gatewayId, fiftyPlus: result.fiftyPlus, seventyPlus: result.seventyPlus, eightyPlus: result.eightyPlus, startTime: result.startTime}))
+                await inTransitModel.findOne(filter).then(async result => {
+                    let longitude =  [];
+                    let latitude = [];
+                    let tempCeil = [];
+                    let tempSide = [];
+                    let avgTemp = [];
+                    let time = [];
+                    await statusData.find(filter).forEach(statusUpdate => {
+                        longitude.push(statusUpdate.longitude);
+                        latitude.push(statusUpdate.latitude);
+                        tempCeil.push(statusUpdate.tempCeil);
+                        tempSide.push(statusUpdate.tempSide);
+                        avgTemp.push((statusUpdate.tempCeil + statusUpdate.tempSide)/2.0);
+                        time.push(statusUpdate.updatedAt);
+                    })
+                    var data = [{
+                        type: 'scattergeo',
+                        mode: 'markers',
+                        lon: longitude,
+                        lat: latitude,
+                        marker: {
+                            size: 2
+                        },
+                        name: 'Status Update Points',
+                    }];
+                    var layout = {
+                        title: 'Status Update Points',
+                        font: {
+                            family: 'Droid Serif, serif',
+                            size: 6
+                        }
+                    };
+                    return resolve(res.render('plot', {gatewayId: req.body.gatewayId, fiftyPlus: result.fiftyPlus, seventyPlus: result.seventyPlus, eightyPlus: result.eightyPlus, startTime: result.startTime}, function (err, html) {
+                        let plot = html.getElementById('plot');
+                        Plotly.newPlot(plot, data, layout);
+                        res.send(html);
+                    }));
                 })
                 .catch(error => {
                     console.log(error)
@@ -18,15 +53,54 @@ router.post('/', async function(req, res, next) {
                 })
             }
             else {
-                return resolve(res.render('plot', {gatewayId: req.body.gatewayId, fiftyPlus: result.fiftyPlus, seventyPlus: result.seventyPlus, eightyPlus: result.eightyPlus, startTime: result.startTime}))
-            }
-                
+                await inTransitModel.findOne(filter).then(async result => {
+                    let longitude =  [];
+                    let latitude = [];
+                    let tempCeil = [];
+                    let tempSide = [];
+                    let avgTemp = [];
+                    let time = [];
+                    await statusData.find(filter).then(result2 => {
+                        result2.forEach(statusUpdate => {
+                            longitude.push(statusUpdate.longitude);
+                            latitude.push(statusUpdate.latitude);
+                            tempCeil.push(statusUpdate.tempCeil);
+                            tempSide.push(statusUpdate.tempSide);
+                            avgTemp.push((statusUpdate.tempCeil + statusUpdate.tempSide)/2.0);
+                            time.push(statusUpdate.updatedAt);
+                        })
+                        var data = [{
+                            type: 'scattergeo',
+                            mode: 'markers',
+                            lon: longitude,
+                            lat: latitude,
+                            marker: {
+                                size: 2
+                            },
+                            name: 'Status Update Points',
+                        }];
+                        var layout = {
+                            title: 'Status Update Points',
+                            font: {
+                                family: 'Droid Serif, serif',
+                                size: 6
+                            }
+                        };
+                        console.log(data);
+                    })
+                    return resolve(res.render('plot', {gatewayId: req.body.gatewayId, fiftyPlus: result.fiftyPlus, seventyPlus: result.seventyPlus, eightyPlus: result.eightyPlus, startTime: result.startTime}, function (err, html) {
+                        console.log(html);
+                        res.send(html);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    }))
+            })
         })
         .catch(error => {
             console.log(error)
             return reject(error)
         })
     })
- })
-
+})
 module.exports = router;
